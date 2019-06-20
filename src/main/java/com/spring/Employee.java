0ea9b03a558ed60358e15c7c9487fc758bd38112
shipra <shipratwarneel@gmail.com>
2019-06-20 04:16:20 -0500

    little_modification

--- src/main/java/com/spring/Employee.java
+++ src/main/java/com/spring/Employee.java
@@ -1,6 +1,18 @@
 package com.spring;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class Employee {
- //shipra
+ 
+	@RequestMapping("/welcome")
+	public ModelAndView demo_Method(){
+		
+		 ModelAndView mod= new ModelAndView("HelloPage");
+		 mod.addObject("display", "This is my first Sprring MVC Projet");
+		 return mod;
+	}
 	
 }
---
+++ src/main/webapp/HelloPage.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+
+<h1>Hello World!</h1>
+<h2>${display}</h2>
+</body>
+</html>
